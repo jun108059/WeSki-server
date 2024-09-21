16f1ee16aef9fb00a71013a2f5967b609e8c5504
YoungJun Park <youngjun108059@gmail.com>
2024-09-21 23:45:49 +0900

    🎉 Create Spring boot project

---
+++ src/main/kotlin/nexters/weski/WeskiApplication.kt
@@ -0,0 +1,11 @@
+package nexters.weski
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WeskiApplication
+
+fun main(args: Array<String>) {
+    runApplication<WeskiApplication>(*args)
+}
---
+++ src/test/kotlin/nexters/weski/WeskiApplicationTests.kt
@@ -0,0 +1,13 @@
+package nexters.weski
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WeskiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
