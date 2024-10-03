e0219548a50ae712c3f16ce1010101b3e8c57c0e
YoungJun Park <youngjun108059@gmail.com>
2024-10-04 01:07:20 +0900

    ♻️ Refactoring : ski resort code

--- src/test/kotlin/nexters/weski/ski_resort/SkiResortControllerTest.kt
+++ src/test/kotlin/nexters/weski/ski_resort/SkiResortControllerTest.kt
@@ -1,16 +1,27 @@
 package nexters.weski.ski_resort
 
+import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
+import nexters.weski.common.config.JpaConfig
+import nexters.weski.weather.SimpleCurrentWeatherDto
+import nexters.weski.weather.WeeklyWeatherDto
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
-
-import com.ninjasquad.springmockk.MockkBean
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(SkiResortController::class)
+@ComponentScan(
+    excludeFilters = [ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = [JpaConfig::class]
+    )]
+)
 class SkiResortControllerTest @Autowired constructor(
     private val mockMvc: MockMvc
 ) {
@@ -20,12 +31,23 @@ class SkiResortControllerTest @Autowired constructor(
 
     @Test
     fun `GET api_ski-resorts should return list of ski resorts`() {
+        val currentWeather = SimpleCurrentWeatherDto(-1, "맑음")
+        val weeklyWeather = listOf(
+            WeeklyWeatherDto("월", 5, -3, "맑음"),
+            WeeklyWeatherDto("화", 6, -2, "맑음"),
+            WeeklyWeatherDto("수", 7, -1, "맑음"),
+            WeeklyWeatherDto("목", 8, 0, "맑음"),
+            WeeklyWeatherDto("금", 9, 1, "맑음"),
+            WeeklyWeatherDto("토", 10, 2, "맑음"),
+            WeeklyWeatherDto("일", 11, 3, "맑음")
+        )
         // Given
         val skiResorts = listOf(
-            SkiResortDto(1, "스키장 A", ResortStatus.운영중, "2023-12-01", "2024-03-01", 5, 10),
-            SkiResortDto(2, "스키장 B", ResortStatus.예정, "2023-12-15", null, 0, 8)
+            SkiResortResponseDto(1, "스키장 A", ResortStatus.운영중.name, 3, currentWeather, weeklyWeather),
+            SkiResortResponseDto(2, "스키장 B", ResortStatus.운영중.name, 4, currentWeather, weeklyWeather),
         )
-        every { skiResortService.getAllSkiResorts() } returns skiResorts
+        every { skiResortService.getAllSkiResortsAndWeather() } returns skiResorts
+
 
         // When & Then
         mockMvc.perform(get("/api/ski-resorts"))
--- src/test/kotlin/nexters/weski/ski_resort/SkiResortServiceTest.kt
+++ src/test/kotlin/nexters/weski/ski_resort/SkiResortServiceTest.kt
@@ -2,13 +2,21 @@ package nexters.weski.ski_resort
 
 import io.mockk.every
 import io.mockk.mockk
+import nexters.weski.weather.CurrentWeatherRepository
+import nexters.weski.weather.DailyWeatherRepository
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class SkiResortServiceTest {
 
     private val skiResortRepository: SkiResortRepository = mockk()
-    private val skiResortService = SkiResortService(skiResortRepository)
+    private val currentWeatherRepository: CurrentWeatherRepository = mockk()
+    private val dailyWeatherRepository: DailyWeatherRepository = mockk()
+    private val skiResortService = SkiResortService(
+        skiResortRepository,
+        currentWeatherRepository,
+        dailyWeatherRepository,
+    )
 
     @Test
     fun `getAllSkiResorts should return list of SkiResortDto`() {
@@ -18,9 +26,11 @@ class SkiResortServiceTest {
             SkiResort(2, "스키장 B", ResortStatus.예정, null, null, 0, 8)
         )
         every { skiResortRepository.findAll() } returns skiResorts
+        every { currentWeatherRepository.findBySkiResortResortId(any()) } returns null
+        every { dailyWeatherRepository.findAllBySkiResortResortId(any()) } returns emptyList()
 
         // When
-        val result = skiResortService.getAllSkiResorts()
+        val result = skiResortService.getAllSkiResortsAndWeather()
 
         // Then
         assertEquals(2, result.size)
