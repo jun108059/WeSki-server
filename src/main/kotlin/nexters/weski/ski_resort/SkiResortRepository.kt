9f3c116f6c0593229f9886aac97d3834bf0b0c74
YoungJun Park <youngjun108059@gmail.com>
2024-11-18 13:45:41 +0900

    ✨ Change: sorting criteria for ski resort query (by ID → by opening date)
    #25

--- src/main/kotlin/nexters/weski/ski_resort/SkiResortRepository.kt
+++ src/main/kotlin/nexters/weski/ski_resort/SkiResortRepository.kt
@@ -2,4 +2,6 @@ package nexters.weski.ski_resort
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface SkiResortRepository : JpaRepository<SkiResort, Long>
+interface SkiResortRepository : JpaRepository<SkiResort, Long> {
+    fun findAllByOrderByOpeningDateAsc(): List<SkiResort>
+}
