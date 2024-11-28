2dcf4ad5a65fb799deadd296004a81c3df94bab1
YoungJun Park <youngjun108059@gmail.com>
2024-11-28 20:20:34 +0900

    ✨ Add: slope and webcam id
    #22

--- src/main/kotlin/nexters/weski/slope/SlopeDto.kt
+++ src/main/kotlin/nexters/weski/slope/SlopeDto.kt
@@ -1,24 +1,28 @@
 package nexters.weski.slope
 
 data class SlopeDto(
+    val slopeId: Long,
     val name: String,
     val difficulty: String,
     val isDayOperating: Boolean,
     val isNightOperating: Boolean,
     val isLateNightOperating: Boolean,
     val isDawnOperating: Boolean,
-    val isMidnightOperating: Boolean
+    val isMidnightOperating: Boolean,
+    val webcamNo: Int?
 ) {
     companion object {
         fun fromEntity(entity: Slope): SlopeDto {
             return SlopeDto(
+                slopeId = entity.id,
                 name = entity.name,
                 difficulty = entity.difficulty.name,
                 isDayOperating = entity.isDayOperating,
                 isNightOperating = entity.isNightOperating,
                 isLateNightOperating = entity.isLateNightOperating,
                 isDawnOperating = entity.isDawnOperating,
-                isMidnightOperating = entity.isMidnightOperating
+                isMidnightOperating = entity.isMidnightOperating,
+                webcamNo = entity.webcamNumber
             )
         }
     }
