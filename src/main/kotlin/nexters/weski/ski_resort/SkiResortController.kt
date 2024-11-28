c05e9b3f1019236d7d7b45243181cd0aca19b2e7
YoungJun Park <youngjun108059@gmail.com>
2024-11-28 20:29:03 +0900

    ✨ Add: GET resort info API
    #22

--- src/main/kotlin/nexters/weski/ski_resort/SkiResortController.kt
+++ src/main/kotlin/nexters/weski/ski_resort/SkiResortController.kt
@@ -1,8 +1,10 @@
 package nexters.weski.ski_resort
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "전체 스키장 정보 API", description = "전체 스키장 날씨 및 슬로프 정보 관련")
@@ -14,5 +16,14 @@ class SkiResortController(
     @GetMapping("/api/ski-resorts")
     fun getAllSkiResorts(): List<SkiResortResponseDto> {
         return skiResortService.getAllSkiResortsAndWeather()
+    }
+
+    @Operation(summary = "날씨와 슬로프 정보를 포함한 특정 스키장 데이터를 조회하는 API")
+    @GetMapping("/api/ski-resort/{resortId}")
+    fun getSkiResort(
+        @Parameter(description = "스키장 ID", example = "1")
+        @PathVariable resortId: Long
+    ): SkiResortResponseDto {
+        return skiResortService.getSkiResortAndWeather(resortId)
     }
 }
